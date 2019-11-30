7cf667d9ad59f0d4b4824ae6890439c02c1d9973
Jordan Evans <jevans17@uoguelph.ca>
2019-11-30 10:45:10 -0500

    Added matrix visual on front end

--- static/index.js
+++ static/index.js
@@ -6,8 +6,25 @@ $(document).ready(()=> {
             url: '/generateMatrix',
             success: (data) => {
                 console.log(data)
+                createMatrix(data.matrix);
             }
         })
     });
-
 });
+
+function createMatrix(matrix) {
+    let string = '';
+    for(let row = 0; row < matrix.length; row++) {
+        string += '<div class=\'row letter_row\'>';
+        for(let col = 0; col < matrix[0].length; col++) {
+            string += `<div class='letter'><span id='row_${row}_col_${col}'>${matrix[row][col].toUpperCase()}</span></div>`
+        }
+        string += '</div>'
+    }
+    $('#content_container').html(string);
+
+    var my_css_class = { float : 'left', width: `${Math.floor(800 / matrix.length)}px` };
+
+    $(".letter").css(my_css_class);
+
+}
